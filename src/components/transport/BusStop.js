75074f0a06b6e5d506440691c9ce0a37fa97da6d
Alessandro Arosio <alessandro.arosio1@gmail.com>
2021-12-23 16:27:37 +0000

    BusStop now shows all the stops in to a collapsible accordion for better readability

--- src/components/transport/BusStop.js
+++ src/components/transport/BusStop.js
@@ -1,24 +1,31 @@
-import {Col, Row} from "react-bootstrap";
+import {Accordion, Col, Row} from "react-bootstrap";
 import BusService from "./BusService";
 
 const BusStop = props => {
     return (
-        <li className="list-group-item">
-            <h3>{props.stopName}</h3>
-            <Row>
-                <Col>
-                    <ul className="list-group">
-                        {props.busInfos.map(bus => (
-                            <BusService
-                                key={bus.busNumber}
-                                busNumber={bus.busNumber}
-                                destination={bus.busRoute}
-                                busTimes={bus.busTimes} />
-                        ))}
-                    </ul>
-                </Col>
-            </Row>
-        </li>
+        <Accordion>
+            <Accordion.Item eventKey="0">
+                <Accordion.Header><strong>{props.stopName}</strong></Accordion.Header>
+                <Accordion.Body>
+                    <li className="list-group-item">
+                        <Row>
+                            <Col>
+                                <ul className="list-group">
+                                    {props.busInfos.map(bus => (
+                                        <BusService
+                                            key={bus.busNumber}
+                                            busNumber={bus.busNumber}
+                                            destination={bus.busRoute}
+                                            busTimes={bus.busTimes}/>
+                                    ))}
+                                </ul>
+                            </Col>
+                        </Row>
+                    </li>
+                </Accordion.Body>
+            </Accordion.Item>
+        </Accordion>
+
     );
 };
 
