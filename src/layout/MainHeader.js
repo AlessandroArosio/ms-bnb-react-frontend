00ef866d82aa658216b0af97ba5ac7a09092c851
Alessandro Arosio <alessandro.arosio1@gmail.com>
2021-12-23 11:01:33 +0000

    Main header now expands or collapses automatically on click

--- src/layout/MainHeader.js
+++ src/layout/MainHeader.js
@@ -1,16 +1,22 @@
 import {NavLink} from "react-router-dom";
 import {Container, Nav, Navbar} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {useState} from "react";
 
 const MainHeader = () => {
+    const [expanded, setExpanded] = useState(false);
+
+    const navbarHandler = () => {
+      setExpanded(!expanded);
+    };
 
     return (
         <header>
-            <Navbar bg="dark" variant="dark" expand="sm">
+            <Navbar bg="dark" variant="dark" expand="sm" expanded={expanded}>
                 <Container>
                     <NavLink className="navbar-brand" to="/">AlbaBnb</NavLink>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={navbarHandler}/>
+                    <Navbar.Collapse id="basic-navbar-nav" onClick={navbarHandler}>
                         <Nav className="me-auto">
                             <NavLink className="nav-link" to="/booking">Booking</NavLink>
                             <NavLink className="nav-link" to="/transport">Transport</NavLink>
